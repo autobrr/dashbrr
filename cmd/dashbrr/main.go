0b2455ac6419c9952991d45ffdc2ac6570326645
soup <soup@r4tio.dev>
2024-11-08 06:32:04 +0100

    refactor: move logger setup from main.go to internal/logger package (#9)

--- cmd/dashbrr/main.go
+++ cmd/dashbrr/main.go
@@ -9,17 +9,16 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
-	"strings"
 	"syscall"
 	"time"
 
 	"github.com/gin-gonic/gin"
-	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 
 	"github.com/autobrr/dashbrr/internal/api/middleware"
 	"github.com/autobrr/dashbrr/internal/api/routes"
 	"github.com/autobrr/dashbrr/internal/database"
+	"github.com/autobrr/dashbrr/internal/logger"
 	"github.com/autobrr/dashbrr/internal/services"
 	"github.com/autobrr/dashbrr/web"
 )
@@ -32,32 +31,7 @@ var (
 )
 
 func init() {
-	colors := map[string]string{
-		"trace": "\033[36m", // Cyan
-		"debug": "\033[33m", // Yellow
-		"info":  "\033[34m", // Blue
-		"warn":  "\033[33m", // Yellow
-		"error": "\033[31m", // Red
-		"fatal": "\033[35m", // Magenta
-		"panic": "\033[35m", // Magenta
-	}
-
-	output := zerolog.ConsoleWriter{
-		Out:     os.Stdout,
-		NoColor: false,
-		FormatLevel: func(i interface{}) string {
-			level, ok := i.(string)
-			if !ok {
-				return "???"
-			}
-			color := colors[level]
-			if color == "" {
-				color = "\033[37m" // Default to white
-			}
-			return color + strings.ToUpper(level) + "\033[0m"
-		},
-	}
-	log.Logger = zerolog.New(output).With().Timestamp().Logger()
+	logger.Init()
 }
 
 func main() {
