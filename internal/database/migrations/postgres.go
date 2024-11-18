9e1d924c1587f86a2a01a48b50c7bcd29bc5ebd5
ze0s <43699394+zze0s@users.noreply.github.com>
2024-11-18 17:00:57 +0100

    feat(database): add migrations (#41)
    * feat(database): add migration system
    
    * feat(database): update sqlite migration
    
    * feat(database): update migrator
    
    ---------
    
    Co-authored-by: soup <soup@r4tio.dev>

---
+++ internal/database/migrations/postgres.go
@@ -0,0 +1,46 @@
+// Copyright (c) 2024, s0up and the autobrr contributors.
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+package migrations
+
+import (
+	"database/sql"
+
+	"github.com/autobrr/dashbrr/pkg/migrator"
+
+	"github.com/dcarbone/zadapters/zstdlog"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+func PostgresMigrator(db *sql.DB) error {
+	migrate := migrator.NewMigrate(
+		db,
+		migrator.WithEmbedFS(SchemaMigrations),
+		migrator.WithLogger(zstdlog.NewStdLoggerWithLevel(log.With().Str("module", "database-migrations").Logger(), zerolog.DebugLevel)),
+	)
+
+	migrate.Add(
+		&migrator.Migration{
+			Name: "000_base_schema",
+			File: "postgres_schema.sql",
+		},
+		&migrator.Migration{
+			Name: "001_add_service_access_url",
+			RunTx: func(db *sql.Tx) error {
+				_, err := db.Exec("ALTER TABLE service_configurations ADD COLUMN access_url TEXT")
+				if err != nil {
+					return err
+				}
+				return nil
+			},
+		},
+	)
+
+	err := migrate.Migrate()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
